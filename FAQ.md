d74db6a31ba70072da94189766250d5b4030d22c
Lionel Alves <alves.lionel5@gmail.com>
2017-11-15 21:04:13 +0100

    sources -> static-and-dynamic-libs Add `Loader` which will be a basic iOS app to load the frameworks created.

---
+++ FAQ.md
@@ -0,0 +1,8 @@
+> `dyld: Library not loaded: @rpath ... Reason: image not found`
+
+`LD_RUNPATH_SEARCH_PATHS`
+
+> `ld: framework not found ...`
+
+`FRAMEWORK_SEARCH_PATHS`
+
---
+++ frameworks/Loader/Loader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Loader
+//
+//  Created by Lionel Alves on 15/11/2017.
+//  Copyright © 2017 Lionel Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
